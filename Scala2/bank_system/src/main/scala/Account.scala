ab5d29b112c65274544bfe4d3022cc2fcbd61689
Jakob Holkestad Molnes <jakob.holkestad.molnes@gmail.com>
2024-11-01 14:48:54 +0100

    complete 1.3

--- Scala2/bank_system/src/main/scala/Account.scala
+++ Scala2/bank_system/src/main/scala/Account.scala
@@ -4,12 +4,22 @@ class Account(val code : String, val balance: Double) {
     // TODO
     // Implement functions. Account should be immutable.
     // Change return type to the appropriate one
-    def withdraw(amount: Double) : Account = {
-        new Account(code, balance-amount)
+    def withdraw(amount: Double) : Either[String, Account] = {
+        if (amount < 0) {
+            Left("Withdrawal amount cannot be negative")
+        } else if (amount > balance) {
+            Left("Insufficient funds")
+        } else {
+            Right(new Account(code, balance-amount))
+        }
     }
 
-    def deposit (amount: Double) : Account = {
-        new Account(code, balance+amount)
+    def deposit (amount: Double) : Either[String, Account] = {
+        if (amount < 0) {
+            Left("Deposit amount cannot be negative")
+        } else {
+            Right(new Account(code, balance+amount))
+        }
     }
 
 }
