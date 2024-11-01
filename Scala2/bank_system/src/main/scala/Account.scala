1ff981a87119aa4264ab3d5df03b3683e1c54c63
Jakob Holkestad Molnes <jakob.holkestad.molnes@gmail.com>
2024-11-01 14:45:38 +0100

    complete 1.2

--- Scala2/bank_system/src/main/scala/Account.scala
+++ Scala2/bank_system/src/main/scala/Account.scala
@@ -4,8 +4,12 @@ class Account(val code : String, val balance: Double) {
     // TODO
     // Implement functions. Account should be immutable.
     // Change return type to the appropriate one
-    def withdraw(amount: Double) : Unit = ???
+    def withdraw(amount: Double) : Account = {
+        new Account(code, balance-amount)
+    }
 
-    def deposit (amount: Double) : Unit = ???
+    def deposit (amount: Double) : Account = {
+        new Account(code, balance+amount)
+    }
 
 }
