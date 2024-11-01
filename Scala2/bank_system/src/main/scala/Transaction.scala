c273eadae1bbc95ee98f3d6a326b7e59dcec0d5d
Jakob Holkestad Molnes <jakob.holkestad.molnes@gmail.com>
2024-11-01 14:41:40 +0100

    implement task 1.1

--- Scala2/bank_system/src/main/scala/Transaction.scala
+++ Scala2/bank_system/src/main/scala/Transaction.scala
@@ -4,20 +4,36 @@ object TransactionStatus extends Enumeration {
 
 class TransactionPool {
 
+    val pool = new scala.collection.mutable.Queue[Transaction]()
+
      // Remove and the transaction from the pool
-    def remove(t: Transaction): Boolean = ???
+    def remove(t: Transaction): Boolean = {
+        pool.dequeueFirst(_ == t) match {
+            case Some(_) => true
+            case None => false
+        }
+    }
 
     // Return whether the queue is empty
-    def isEmpty: Boolean = ???
+    def isEmpty: Boolean = {
+        pool.isEmpty
+    }
 
     // Return the size of the pool
-    def size: Integer = ???
+    def size: Integer = {
+        pool.size
+    }
 
     // Add new element to the back of the queue
-    def add(t: Transaction): Boolean = ???
+    def add(t: Transaction): Boolean = {
+        pool.enqueue(t)
+        true
+    }
 
     // Return an iterator to allow you to iterate over the queue
-    def iterator : Iterator[Transaction] = ???
+    def iterator : Iterator[Transaction] = {
+        pool.iterator
+    }
 
 }
 
